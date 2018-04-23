51650bed72930e02ea9754136c9785d289b74e33
pat1324 <pguo6@student.monash.edu>
2018-04-23 21:22:38 +1000

    Modified to enable testing on Ganache

--- src/truffle-config.js
+++ src/truffle-config.js
@@ -1,4 +1,12 @@
+/*
+Modified to work with Ganache as previously this file was empty and Ganache couldn't be accessed
+*/
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+   networks: {
+   development: {
+   host: "localhost",
+   port: 7545,
+   network_id: "*" // Match any network id
+  }
+ }
 };
